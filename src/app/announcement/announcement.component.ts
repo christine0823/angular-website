45f7293e2683de2c49c94045fa3f5bd913121708
christinehsiao <ch25814@gmail.com>
2018-12-01 00:11:51 +0800

    Add new files

---
+++ src/app/announcement/announcement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement',
+  templateUrl: './announcement.component.html',
+  styles: []
+})
+export class AnnouncementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
