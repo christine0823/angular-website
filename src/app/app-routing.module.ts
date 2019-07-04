939063e0d21263a71142b96b17d428ea08432a05
christinehsiao <ch25814@gmail.com>
2019-07-04 21:41:29 +0800

    update version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DocumentComponent } from './document/document.component';
+import { InformationComponent } from './information/information.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'announcement', component: AnnouncementComponent},
-  { path: 'document', component: DocumentComponent }
+  { path: 'document', component: DocumentComponent },
+  { path: 'information', component: InformationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,17 @@ import { AnnouncementComponent } from './announcement/announcement.component';
 import { DocumentComponent } from './document/document.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PdfViewerModule} from 'ng2-pdf-viewer';
 import * as $ from "jquery";
+import { InformationComponent } from './information/information.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -17,13 +27,19 @@ import * as $ from "jquery";
     AnnouncementComponent,
     DocumentComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PdfViewerModule,
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
